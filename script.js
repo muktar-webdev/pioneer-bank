29982b2404413328a56e721ebe2b35307bc97845
Muktar2298 <muktar15-2298@diu.edu.bd>
2021-01-26 11:29:51 +0600

    Pioneer-Bank

---
+++ script.js
@@ -0,0 +1,7 @@
+const loginBtn = document.getElementById("login");
+loginBtn.addEventListener('click',function(){
+    const loginArea = document.getElementById('login-area');
+    loginArea.style.display = "none";
+    const transactionArea = document.getElementById("transaction-area");
+    transactionArea.style.display = "block";
+})
